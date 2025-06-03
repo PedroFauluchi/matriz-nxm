75437e0b6f069dd29eda0972eb2d743232ef7393
PedroFauluchi <pedrofauluchi@gmail.com>
2025-06-03 14:11:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 function handleSubmit(event){
     event.preventDefault();
-    let f=document.querySelector('#filas').value;
-    let c=document.querySelector('#columnas').value;
+    let f=document.event.target('filas').value;
+    let c=document.event.target('columnas').value;
     let filas=0;
     while(filas<f){
         for(i=0;i<c;i++){
          let div=document.createElement('div');
         event.target.append(div);
         div.classList.add('square');
+        div.textContent(i+1);
+        div.style.gridTemplate=`repeat(${f}, 1fr)/repeat(${c}, 1fr)`
         }
         filas++;
     }
